10d90747883ad38dfe4e7ff7c1a345d6e3f217d8
Justin Wong <justintwong@berkeley.edu>
2019-09-30 14:35:36 -0700

    Test2

---
+++ Justin's Test/Test Project/Assets/LookAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    public GameObject player;      
+
+
+    private Vector3 offset;
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.LookAt(target);
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Justin's Test/Test Project/Assets/MovementWASD.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementWASD : MonoBehaviour
+{
+    public float speed = 8f;
+    public float jumpheight = 15;
+    Rigidbody rb;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log(col.gameObject.tag);
+        if (isGrounded == false)
+        {
+            isGrounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-speed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, -speed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.C) && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, jumpheight, 0), ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
